0bd92290e8b40598a3396644c1477001e387ccd9
Antonello Giannitrapani <giannitrapani@dii.unisi.it>
2014-12-15 15:35:56 +0100

    A working version

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
 ##
+## The function makeCacheMatrix() creates a data structure for speeding up 
+## the computation of a matrix inverse. 
 ##
-## Caveats: The matrix supplied is assumed to be always invertible.
+## The function cacheSolve() returns the matrix inverse and caches the 
+## result for subsequent calls to the function
+##
 
-
-## Write a short comment describing this function
+##
+## makeCacheMatrix(x) takes as input a matrix x and returns a list of 
+## functions for setting/getting the matrix entries (set/get functions) and for 
+## setting/getting the matrix inverse (setinv/getinv functions).
+##
+## NB. The matrix supplied is assumed to be always invertible.
+##
 makeCacheMatrix <- function(x = matrix()) {
+        
+        inv <- NULL
+        
+        # Update the matrix according to y
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        
+        # Return the matrix
+        get <- function() {
+                x
+        }
+        
+        # Set the matrix inverse according to ii
+        setinv <- function(xi){
+                inv <<- xi
+        }
+        
+        # Return the matrix inverse
+        getinv <- function(){
+                inv
+        }
+        
+        # Return the newly built object
+        list( set=set, get=get, setinv=setinv, getinv=getinv)
 
 }
 
 
-## Write a short comment describing this function
 
+##
+## cacheSolve(x, ...) takes as input a matrix represented as object x built with 
+## the makeCacheMatrix() function and returns its inverse. The result is cached
+## for speeding up subsequent calls by updating the object x
+##
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+
+        if ( is.null(x$getinv()) ){
+                # Inverse must be computed and cache updated
+                x$setinv(solve(x$get()))
+                message("Computing the inverse and updating cache")
+        }
+        
+        # Returns the inverse
+        x$getinv()
+                
+                
 }
